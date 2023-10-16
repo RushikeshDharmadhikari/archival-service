53eb258a0304f8cf520ad0b86637d21bc8967a17
RushikeshDharmadhikari <rushikeshdharmadhikari@Rushikeshs-MacBook-Air.local>
2023-10-16 17:11:14 +0530

    Fixed authentication issues

--- src/main/java/com/archival/archivalservice/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/archival/archivalservice/JwtAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +15,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
-			org.springframework.security.core.AuthenticationException authException)
+			AuthenticationException authException)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 	}
 }
